1c6dc3de5db66043186f1c48704fb725f05956c3
Per Olav Flaten <perolavflaten@live.no>
2016-09-09 18:20:17 +0200

    Added script for intensity transformation

---
+++ Image Processing/Assignment01/intensity_transformations.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import misc
+
+def negative(image):
+    return 255 - image
+
+def gamma(image, c, gamma):
+    imagelog = image.astype(np.float32)
+    imagelog /= 255
+    imagelog = c * (imagelog ** gamma)
+
+    return imagelog
+
+# Load color image
+image = misc.imread('../images/5.1.10-aerial.tiff', mode = 'L')
+#image = misc.imread('../images/5.2.09-aerial.tiff', mode = 'L')
+
+# Apply intensity transformation to image
+image = negative(image)
+#image = gamma(image, 1, 5)
+
+# Show image
+plt.figure()
+plt.imshow(image, cmap=plt.cm.gray)
+plt.show()
